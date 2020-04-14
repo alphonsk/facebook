5b7fab36efa876e6d248821e289ab19a20742496
GwG <gwg@GwGs-MacBook-Air.local>
2020-04-14 10:52:42 -0400

    added basic authentication

--- src/main/java/com/george/facebook/controller/AuthController.java
+++ src/main/java/com/george/facebook/controller/AuthController.java
@@ -1,6 +1,9 @@
 package com.george.facebook.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,4 +13,24 @@ public class AuthController {
     String admin() {
         return "admin";
     }
+
+    @GetMapping("/login")
+    public String getLogin(){
+        return "login";
+    }
+
+
+    @GetMapping("/signup")
+    public String getSignup(){
+        return "signup";
+    }
+
+    @PostMapping("/login")
+    public String getin(){
+        return "posts";
+    }
+
+
+
+
 }
